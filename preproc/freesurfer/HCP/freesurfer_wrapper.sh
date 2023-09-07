ce9c9430ea226b80f99ca89f65c467c1e604f2d3
peter3200 <40050100+peter3200@users.noreply.github.com>
2023-09-07 13:11:35 -0600

    Add files via upload

--- preproc/freesurfer/HCP/freesurfer_wrapper.sh
+++ preproc/freesurfer/HCP/freesurfer_wrapper.sh
@@ -4,7 +4,7 @@ genDir=/fslgroup/fslg_spec_networks/compute
 codeDir=${genDir}/code/HCP_analysis/freesurfer
 dataDir=/fslgroup/grp_proc/compute/HCP_analysis/HCP_download
 
-for subj in `cat ${codeDir}/subjids/CBIG_HCP_subject_list.txt`; do
+for subj in `cat ${codeDir}/subjids/subjids.txt`; do
     mkdir -p ${codeDir}/logfiles
     sbatch \
         -o ${codeDir}/logfiles/output_sub-${subj}.txt \
